c6d7a7b3e37cf9a57a1b10618b247c8b6900974a
djsy01 <dlsgh8884@naver.com>
2025-09-22 18:50:09 +0900

    env세팅 수정

--- server/src/main/java/ticketingsitedemo/server/ServerApplication.java
+++ server/src/main/java/ticketingsitedemo/server/ServerApplication.java
@@ -5,12 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @MapperScan("ticketingsitedemo.server.mapper")
 @EnableJpaRepositories("ticketingsitedemo.server.repository")
 public class ServerApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        System.out.println("✅ DB_HOST from .env: " + System.getProperty("DB_HOST"));
         SpringApplication.run(ServerApplication.class, args);
 
     }
